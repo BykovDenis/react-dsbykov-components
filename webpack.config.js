ab4c0ab9b3409bca253bb7ecb90001ba06142834
Denis <stivenrain@gmail.com>
2018-10-21 17:22:07 +0300

    optimization bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var nodeExternals = require('webpack-node-externals');
+
 module.exports = {
   entry: './example/app.js',
   output: {
@@ -17,6 +19,8 @@ module.exports = {
     'react-dom': 'ReactDOM',
     '@material-ui/core': '@material-ui/core',
     'prop-types': 'prop-types',
+    target: 'node', // in order to ignore built-in modules like path, fs, etc.
+    externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   },
   devtool: "source-map"
 };
